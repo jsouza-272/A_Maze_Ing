0680a7db3b3b6a1d04815a58aad82604286d44c7
Joao Souza <jvpvpsouza7@gmail.com>
2026-03-19 15:00:35 +0000

    rename

--- MazeGenerator/test.py
+++ MazeGenerator/test.py
@@ -5,7 +5,7 @@ from .Astar import Astar
 if __name__ == "__main__":
     entry = (1, 1)
     exit = (19, 14)
-    generator = MazeGenerator(20, 25, entry, exit, True)
+    generator = MazeGenerator(20, 15, entry, exit, True)
     generator.generate_maze()
     print(generator.get_maze())
     print(Astar().algorithm(generator.maze, entry, exit))
